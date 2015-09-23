117d23f8078332ffba9471fdbd0bebfd63489138
hfloveyy@126.com <hfloveyy@126.com>
2015-09-23 22:13:44 +0800

    make it better

--- myweb.py
+++ myweb.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/hello')
+def hello_world2():
+    return 'Hello World2!'
+
 if __name__ == '__main__':
     app.run(debug=True)
