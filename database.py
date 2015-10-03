eee21298ff3e1ae494d0a0e370c3628762cea70a
hfloveyy@126.com <hfloveyy@126.com>
2015-10-03 20:44:33 +0800

    make it better

--- database.py
+++ database.py
@@ -7,5 +7,5 @@ from myweb import app
 app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://%s:%s@%s:%s/%s' % (sae.const.MYSQL_USER, sae.const.MYSQL_PASS, sae.const.MYSQL_HOST, sae.const.MYSQL_PORT, sae.const.MYSQL_DB) 
 
 
-def db():
-    db = SQLAlchemy(app)
+
+db = SQLAlchemy(app)
