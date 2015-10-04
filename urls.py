167e91d15d4415692c3cd6c38256d6089b2fb118
hfloveyy@126.com <hfloveyy@126.com>
2015-10-04 19:05:51 +0800

    make it better

--- urls.py
+++ urls.py
@@ -29,8 +29,12 @@ def index():
     #admin3 = User(name='Test23', email='test@example.com32', password='123456789032', image='about:blank32')
     #db.session.add(admin3)
     #db.session.commit()
-    admin = User.query.filter_by(name='Test').first()
-    return admin.name
+    #admin = User.query.filter_by(name='Test').first()
+    users = User.query.all()
+    return render_template('test_users.html', users=users)
+
+
+
 
 @app.route('/hello')
 @app.route('/hello/<name>')
