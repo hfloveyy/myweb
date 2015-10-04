278016331c038530327c5e3ce2ff9fa07d68d825
hfloveyy@126.com <hfloveyy@126.com>
2015-10-04 17:52:35 +0800

    make it better

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(50), unique=True)
-    admin = db.Column(db.String(50), unique=True)
+    admin = db.Column(db.Boolean, unique=True)
     name = db.Column(db.String(50), unique=True)
     image = db.Column(db.String(500), unique=True)
     created_at = db.Column(db.DateTime)
