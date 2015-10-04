3fdefafdaf5ed1249fda716cf45935a49c9e6865
hfloveyy@126.com <hfloveyy@126.com>
2015-10-04 12:27:02 +0800

    make it better

--- models.py
+++ models.py
@@ -5,15 +5,40 @@ from database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(50), unique=True)
+    admin = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), unique=True)
+    image = db.Column(db.String(500), unique=True)
+    created_at = db.Column(db.DateTime, unique=True)
 
-    def __init__(self, username, email):
-        self.username = username
+    def __init__(self, email, password, admin, name, image, created_at):
         self.email = email
+        self.password = password
+        self.admin = admin
+        self.name = name
+        self.image = image
+        self.created_at = created_at
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Blog(object):
+    """docstring for Blog"""
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(50), unique=True)
+    user_name = db.Column(db.String(50), unique=True)
+    user_image = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), unique=True)
+    summary = db.Column(db.String(50), unique=True)
+    content = db.Column(db.String(1000), unique=True)
+    created_at = db.Column(db.DateTime, unique=True)
+    def __init__(self, arg):
+        super(Blog, self).__init__()
+        self.arg = arg
+        
+
+
+
+
+
 
 
 
