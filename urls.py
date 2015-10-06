eea58f637e84fc08bece98a21724600a5e634717
hfloveyy@126.com <hfloveyy@126.com>
2015-10-06 14:57:55 +0800

    make it better

--- urls.py
+++ urls.py
@@ -46,10 +46,10 @@ def register_user():
             raise APIValueError('email')
         if not password or not _RE_MD5.match(password):
             raise APIValueError('password')
-        user = User.query.filter_by(email=email).first()
+        '''user = User.query.filter_by(email=email).first()
         if user:
             raise APIError('register:failed', 'email', 'Email is already in use.')
-        '''user = User(name=name, email=email, password=password)
+        user = User(name=name, email=email, password=password)
         db.session.add(user)
         db.session.commit()'''
         return name
