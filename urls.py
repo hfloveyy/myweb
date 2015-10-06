55a541486fc7765e3753848b5256e8a195b14616
hfloveyy@126.com <hfloveyy@126.com>
2015-10-06 15:00:45 +0800

    make it better

--- urls.py
+++ urls.py
@@ -31,7 +31,7 @@ def index():
     blogs = Blog.query.all()
     return render_template('blog.html',blogs=blogs)
 
-
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_MD5 = re.compile(r'^[0-9a-f]{32}$')
 
 @app.route('/register',methods=['GET','POST'])
@@ -44,14 +44,14 @@ def register_user():
             raise APIValueError('name')
         if not email or not _RE_EMAIL.match(email):
             raise APIValueError('email')
-        '''if not password or not _RE_MD5.match(password):
+        if not password or not _RE_MD5.match(password):
             raise APIValueError('password')
         user = User.query.filter_by(email=email).first()
         if user:
             raise APIError('register:failed', 'email', 'Email is already in use.')
         user = User(name=name, email=email, password=password)
         db.session.add(user)
-        db.session.commit()'''
+        db.session.commit()
         return name
     else:
         return render_template('register.html')
