1adde7bff80a03de8d3fb1a066488e2e56b20699
hfloveyy@126.com <hfloveyy@126.com>
2015-10-06 15:18:41 +0800

    make it better

--- urls.py
+++ urls.py
@@ -56,7 +56,7 @@ def register_user():
         user = User(name=name, email=email, password=password)
         db.session.add(user)
         #db.session.commit()
-        return url_for('index')
+        url_for('index')
         #return 'post'
     else:
         return render_template('register.html')
